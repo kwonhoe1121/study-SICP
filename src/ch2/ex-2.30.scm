9321c4dc2459cb3ba3a4eaa3ad899312421f7fd7
kwonhoe <kwonhoe1121@gmail.com>
2024-03-09 15:53:10 +0900

    feat: exercise 2.30

---
+++ src/ch2/ex-2.30.scm
@@ -0,0 +1,18 @@
+(load "./src/lib/util.scm")
+
+(define (square-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (square tree))
+        (else
+          (cons (square-tree (car tree))
+                (square-tree (cdr tree))))))
+
+(define (square-tree tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+           (square-tree sub-tree)
+           (square sub-tree)))
+       tree))
+
+; (display (square-tree (list 1 (list 2 (list 3 4) 5) (list 6 7))))
+; (1 (4 (9 16) 25) (36 49))
