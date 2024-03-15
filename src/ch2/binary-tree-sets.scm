84a33c4c9c7e998fbc50b520eb9cf6e586e67128
kwonhoe <kwonhoe1121@gmail.com>
2024-03-15 16:02:53 +0900

    feat: exercise 2.65

--- src/ch2/binary-tree-sets.scm
+++ src/ch2/binary-tree-sets.scm
@@ -5,6 +5,61 @@
 (define (right-branch tree) (caddr tree))
 (define (make-tree entry left right) (list entry left right))
 
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append 
+       (tree->list-1 
+        (left-branch tree))
+       (cons (entry tree)
+             (tree->list-1 
+              (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list 
+         (left-branch tree)
+         (cons (entry tree)
+               (copy-to-list 
+                (right-branch tree)
+                result-list)))))
+  (copy-to-list tree '()))
+
+(define (list->tree elements)
+  (car (partial-tree 
+        elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size 
+             (quotient (- n 1) 2)))
+        (let ((left-result 
+               (partial-tree ; left tree
+                elts left-size)))
+          (let ((left-tree 
+                 (car left-result))
+                (non-left-elts 
+                 (cdr left-result))
+                (right-size 
+                 (- n (+ left-size 1))))
+            (let ((this-entry ; root
+                   (car non-left-elts))
+                  (right-result 
+                   (partial-tree ; right tree
+                    (cdr non-left-elts)
+                    right-size)))
+              (let ((right-tree
+                     (car right-result))
+                    (remaining-elts 
+                     (cdr right-result)))
+                (cons (make-tree this-entry 
+                                 left-tree 
+                                 right-tree)
+                      remaining-elts))))))))
+
 (define (element-of-set? x set)
   (cond ((null? set) false)
         ((= x (entry set)) true)
@@ -30,3 +85,35 @@
           (entry set)
           (left-branch set)
           (adjoin-set x (right-branch set))))))
+
+; ex-2.65
+
+; O(n)
+
+(define (union-set set1 set2)
+  (let ((list1 (tree->list-2 set1))
+        (list2 (tree->list-2 set2)))
+    (list->tree (union-set list1 list2))))
+
+(define (intersection-set set1 set2)
+  (let ((list1 (tree->list-2 set1))
+        (list2 (tree->list-2 set2)))
+    (list->tree (intersection-set list1 list2))))
+
+; O(n*logn)
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else
+          (union-set (cdr set1)
+                     (adjoin-set (car set1)
+                                 set2)))))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) nil)
+        ((element-of-set? (car set1) set2) (cons (car set1)
+                                                 (intersection-set
+                                                   (cdr set1)
+                                                   set2)))
+        ((else (intersection-set (cdr set1) set2)))))
