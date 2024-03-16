ff7ca6c8a6f38a94a3ed71070b3feee038b90c4d
kwonhoe <kwonhoe1121@gmail.com>
2024-03-16 16:30:38 +0900

    feat: 복소수 표현 방법 추가

---
+++ src/ch2/complex-numbers/complex-numbers.scm
@@ -0,0 +1,21 @@
+(load "./src/lib/util.scm")
+
+(define (real-part z) 
+  (apply-generic 'real-part z))
+(define (imag-part z) 
+  (apply-generic 'imag-part z))
+(define (magnitude z) 
+  (apply-generic 'magnitude z))
+(define (angle z) 
+  (apply-generic 'angle z))
+
+(define (make-from-real-imag x y)
+  ((get 'make-from-real-imag 
+        'rectangular) 
+   x y))
+
+(define (make-from-mag-ang r a)
+  ((get 'make-from-mag-ang 
+        'polar) 
+   r a))
+
