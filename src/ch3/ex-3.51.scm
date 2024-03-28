e24273f6aeb029b046ce34c483f40ce29fbbfb55
kwonhoe <kwonhoe1121@gmail.com>
2024-03-28 19:30:11 +0900

    feat: exercise 3.51

---
+++ src/ch3/ex-3.51.scm
@@ -0,0 +1,24 @@
+(load "./src/lib/stream.scm")
+
+(define (show x)
+  (display-line x)
+  x)
+
+(define x 
+  (stream-map 
+   show 
+   (stream-enumerate-interval 0 10)))
+
+; 0
+
+(stream-ref x 5)
+; 0
+; 1
+; 2
+; 3
+; 4
+; 5
+
+(stream-ref x 7) ; memo-proc
+; 6
+; 7
