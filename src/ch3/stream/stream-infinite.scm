76e9670412742a5298a8492da1db4bfda8a6ef18
kwonhoe <kwonhoe1121@gmail.com>
2024-03-29 17:23:01 +0900

    feat: exercise 3.62

--- src/ch3/stream/stream-infinite.scm
+++ src/ch3/stream/stream-infinite.scm
@@ -54,3 +54,55 @@
           ((divisible? n (stream-car ps)) false)
           (else (iter (stream-cdr ps)))))
   (iter primes))
+
+(define (integrate-series s)
+  (stream-map
+    *
+    (stream-map / ones integers)
+    s))
+
+(define exp-series
+  (cons-stream 
+   1 (integrate-series exp-series)))
+
+(define sine-series
+  (cons-stream
+    0
+    (integrate-series
+      cosine-series)))
+
+(define cosine-series
+  (cons-stream
+    1
+    (integrate-series
+      (scale-stream sine-series -1))))
+
+(define (mul-series s1 s2)
+  (cons-stream
+    (* (stream-car s1) (stream-car s2))
+    (add-streams
+      (mul-series (stream-cdr s1) s2)
+      (scale-stream (stream-cdr s2) (stream-car s1)))))
+
+(define (invert-unit-series series)
+  (define inverted-unit-series
+    (cons-stream 1 (scale-stream (mul-streams (stream-cdr series)
+                                              inverted-unit-series)
+                                 -1)))
+  inverted-unit-series)
+
+(define (div-series s1 s2)
+  (let ((c (stream-car s2)))
+    (if (= c 0)
+        (error "constant term of s2 can't be 0!")
+        (scale-stream
+          (mul-series
+            s1
+            (invert-unit-series
+              (scale-stream
+                s2
+                (/ 1 c))))
+          (/ 1 c)))))
+
+(define tangent-series
+  (div-series sine-series cosine-series))
