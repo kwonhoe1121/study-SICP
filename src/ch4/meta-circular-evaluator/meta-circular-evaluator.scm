79812e889f0ca7bf8776886fb93c65b0575f3049
kwonhoe <kwonhoe1121@gmail.com>
2024-04-04 23:49:02 +0900

    feat: 기본 프로시저 적용 부분 코드 추가

--- src/ch4/meta-circular-evaluator/meta-circular-evaluator.scm
+++ src/ch4/meta-circular-evaluator/meta-circular-evaluator.scm
@@ -314,3 +314,77 @@
                         (cdr vals)))))
     (scan (frame-variables frame)
           (frame-values frame))))
+
+; 기본 프로시저가 돌아가도록 밑바탕에 있는 Lisp 시스템에서 힘을 빌려 쓰는 매커니즘 구현
+(define the-global-environment 
+  (setup-environment))
+
+(define (primitive-procedure? proc)
+  (tagged-list? proc 'primitive))
+
+(define (primitive-implementation proc) 
+  (cadr proc))
+
+(define primitive-procedures
+  (list (list 'car car)
+        (list 'cdr cdr)
+        (list 'cons cons)
+        (list 'null? null?)
+        (list '+ +)
+        (list 'append append))) ; 추가되는 기본 원소들
+
+(define (primitive-procedure-names)
+  (map car primitive-procedures))
+
+(define (primitive-procedure-objects)
+  (map (lambda (proc) 
+         (list 'primitive (cadr proc)))
+       primitive-procedures))
+
+; (define apply-in-underlying-scheme apply)
+
+(define (apply-primitive-procedure proc args)
+  (apply-in-underlying-scheme
+   (primitive-implementation proc) args))
+
+(define input-prompt  ";;; M-Eval input:")
+(define output-prompt ";;; M-Eval value:")
+
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (read)))
+    (let ((output 
+           (eval input 
+                 the-global-environment)))
+      (announce-output output-prompt)
+      (user-print output)))
+  (driver-loop))
+
+(define (prompt-for-input string)
+  (newline) (newline) 
+  (display string) (newline))
+
+(define (announce-output string)
+  (newline) (display string) (newline))
+
+(define (user-print object)
+  (if (compound-procedure? object)
+      (display 
+       (list 'compound-procedure
+             (procedure-parameters object)
+             (procedure-body object)
+             '<procedure-env>))
+      (display object)))
+
+(define the-global-environment 
+  (setup-environment))
+
+(define (setup-environment)
+  (let ((initial-env
+         (extend-environment 
+          (primitive-procedure-names)
+          (primitive-procedure-objects)
+          the-empty-environment)))
+    (define-variable! 'true true initial-env)
+    (define-variable! 'false false initial-env)
+    initial-env))
