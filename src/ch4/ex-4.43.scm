0b590322701a8f3b487862d435ad05b2284812b3
kwonhoe <kwonhoe1121@gmail.com>
2024-04-15 23:23:26 +0900

    feat: exercise 4.43

---
+++ src/ch4/ex-4.43.scm
@@ -0,0 +1,20 @@
+; [참고답안]
+
+(define (father-daughter)
+  (let ((Moore 'Mary)
+        (Barnacle 'Melissa)
+        (Hall (amb 'Gabrielle 'Lorna))
+        (Downing (amb 'Gabrielle 'Lorna 'Rosalind))
+        (Parker (amb 'Lorna 'Rosalind)))
+       (require (cond ((eq? Hall 'Gabrielle) (eq? 'Rosalind Parker))
+                      ((eq? Downing 'Gabrielle) (eq? 'Melissa Parker))
+                      (else false)))
+       (require (distinct? (list Hall Downing Parker)))
+       (list (list 'Barnacle Barnacle)
+             (list 'Moore Moore)
+             (list 'Hall Hall)
+             (list 'Downing Downing)
+             (list 'Parker Parker))))
+
+; ((Barnacle Melissa) (Moore Gabrielle) (Hall Mary) (Downing Rosalind) (Parker Lorna))
+; ((Barnacle Melissa) (Moore Mary) (Hall Gabrielle) (Downing Lorna) (Parker Rosalind))
