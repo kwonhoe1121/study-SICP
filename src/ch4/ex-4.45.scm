eda9d16039f23e491f9ee4670dee4e2247c4de4d
kwonhoe <kwonhoe1121@gmail.com>
2024-04-16 22:51:10 +0900

    feat: exercise 4.45

---
+++ src/ch4/ex-4.45.scm
@@ -0,0 +1,74 @@
+(load "./src/ch4/amb-evaluator/parsing-natural-language.scm")
+
+; "The professor lectures to the student with the cat"
+
+; [참고답안]
+
+;;; Amb-Eval input:
+(parse '(the professor lectures to the student in the class with the cat))
+
+;;; Starting a new problem
+;;; Amb-Eval output:
+(sentence
+  (noun-phrase (articles the) (nouns professor))
+  (verb-phrase (verb-phrase
+                 (verb-phrase (verb lectures)
+                              (pre-phrase (prep to) (noun-phrase (articles the) (nouns student))))
+                 (pre-phrase (prep in) (noun-phrase (articles the) (nouns class))))
+               (pre-phrase (prep with) (noun-phrase (articles the) (nouns cat)))))
+
+;;; Amb-Eval input:
+try-again
+
+;;; Amb-Eval output:
+(sentence (noun-phrase (articles the) (nouns professor))
+          (verb-phrase (verb-phrase (verb lectures)
+                                    (pre-phrase (prep to)
+                                                (noun-phrase (articles the)
+                                                             (nouns student))))
+                       (pre-phrase (prep in)
+                                   (noun-phrase (noun-phrase (articles the) (nouns class))
+                                                (pre-phrase (prep with)
+                                                            (noun-phrase (articles the) (nouns cat)))))))
+
+;;; Amb-Eval input:
+try-again
+
+;;; Amb-Eval output:
+(sentence (noun-phrase (articles the) (nouns professor))
+          (verb-phrase (verb-phrase (verb lectures)
+                                    (pre-phrase (prep to)
+                                                (noun-phrase (noun-phrase (articles the) (nouns student))
+                                                             (pre-phrase (prep in)
+                                                                         (noun-phrase (articles the) (nouns class))))))
+                       (pre-phrase (prep with) (noun-phrase (articles the) (nouns cat)))))
+
+;;; Amb-Eval input:
+try-again
+
+;;; Amb-Eval output:
+(sentence (noun-phrase (articles the) (nouns professor))
+          (verb-phrase (verb lectures)
+                       (pre-phrase (prep to)
+                                   (noun-phrase (noun-phrase (noun-phrase (articles the)
+                                                                          (nouns student))
+                                                             (pre-phrase (prep in)
+                                                                         (noun-phrase (articles the)
+                                                                                      (nouns class))))
+                                                (pre-phrase (prep with) (noun-phrase (articles the) (nouns cat)))))))
+
+;;; Amb-Eval input:
+try-again
+
+;;; Amb-Eval output:
+(sentence (noun-phrase (articles the) (nouns professor))
+          (verb-phrase (verb lectures)
+                       (pre-phrase (prep to)
+                                   (noun-phrase (noun-phrase (articles the)
+                                                             (nouns student))
+                                                (pre-phrase (prep in)
+                                                            (noun-phrase (noun-phrase (articles the)
+                                                                                      (nouns class))
+                                                                         (pre-phrase (prep with)
+                                                                                     (noun-phrase (articles the)
+                                                                                                  (nouns cat)))))))))
