dfc0b095ed4166be64287bc56a07a370cabd63be
kwonhoe <kwonhoe1121@gmail.com>
2024-04-19 00:53:11 +0900

    feat: exercise 4.54

---
+++ src/ch4/ex-4.54.scm
@@ -0,0 +1,20 @@
+; require special form instead of procedure
+
+((require? exp) (analyze-require exp))
+
+(define (require? exp) 
+  (tagged-list? exp 'require))
+
+(define (require-predicate exp) 
+  (cadr exp))
+
+(define (analyze-require exp)
+  (let ((pproc (analyze 
+                (require-predicate exp))))
+    (lambda (env succeed fail)
+      (pproc env
+             (lambda (pred-value fail2)
+                 (if (not (true? pred-value)) 
+                     (fail2) 
+                     (succeed 'ok fail2)) 
+             fail))))
